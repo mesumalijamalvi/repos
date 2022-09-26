da100c149de83d47b2b38cc7d20d54482bfc2473
mesumalijamalvi <mesamjamalvi592@gmail.com>
2022-09-26 03:59:04 -0700

    file3

---
+++ app.js
@@ -0,0 +1,53 @@
+alert("jamalvii here")
+
+// Arithmeical operations
+
+// addition
+
+let m1 = 10
+
+let m2 = 18
+
+
+console.log(m1+m2)
+
+// subtraction
+
+
+let m3 = 10
+
+let m4 = 18
+
+
+console.log(m3-m4)
+
+// multiplication
+
+
+let m5 = 10
+
+let m6 = 18
+
+
+console.log(m5*m6)
+
+// division
+
+
+let m7 = 10
+
+let m8 = 18
+
+
+console.log( m7/m8)
+
+
+
+
+// power
+ 
+let num1 = 5
+
+let num2 = 5
+
+console.log(num1 ** num2)
